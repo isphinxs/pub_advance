3779f2a30c5e9755e3d217c7d161cfcaba0ecc1c
Samantha Ostrowski <isphinxs@hotmail.com>
2021-02-20 16:45:46 -0500

    Add validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 class Author < ActiveRecord::Base
+    validates :name, :password, :username, presence: true
+
     has_secure_password
     has_many :books
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ActiveRecord::Base
-    belongs_to :author
+    validates :name, presence: true
+    belongs_to :author 
+    # validates_associated :author # called upon save; default error message "is invalid" 
 end
