694af0b88e5776a7eb1e87583a1b01dbb79311a4
Samantha Ostrowski <isphinxs@hotmail.com>
2021-02-20 17:01:37 -0500

    Add Helpers module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 require "./config/environment"
 require "sinatra/base"
 
+module Helpers
+    def is_logged_in?(session)
+        !!session[:author_id]
+    end
+
+    def current_author
+        Author.find(session[:author_id])
+    end
+end
+
 class ApplicationController < Sinatra::Base
     configure do
         enable :sessions
@@ -10,4 +20,18 @@ class ApplicationController < Sinatra::Base
     get "/" do
         erb :index
     end
+
+    get "/signup" do
+    
+    end
+    
+    get "/login" do
+        
+    end
+
+    get "/logout" do
+        
+    end
+
+    helpers Helpers
 end
