30ebd7e0521e8f6acf166c6ffb43dfd8feba76ad
Samantha Ostrowski <isphinxs@hotmail.com>
2021-02-20 21:57:59 -0500

    Add new routes to BooksController

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,12 @@ class BooksController < ApplicationController
     end
 
     get "/books/new" do
-        
+        if is_logged_in?(session)
+            @author = current_author
+            erb :"books/new"
+        else
+            redirect "/login"
+        end
     end
 
     get "/books/:id" do # update to title slug?
@@ -23,7 +28,24 @@ class BooksController < ApplicationController
     end
 
     post "/books" do
-        
+        if is_logged_in?(session)
+            title = params[:title]
+            year_published = params[:year_published]
+            advance = params[:advance]
+            if title.blank? || year_published.blank? || advance.blank?
+                redirect "/books/new" # flash, or update to a page with the data already filled out?
+            else
+                book = Book.new(name: title, year_published: year_published, advance: advance, author_id: current_author.id)
+                if book.save
+                    id = book.id
+                    redirect "/books/#{id}"
+                else
+                    redirect "/books/new" # flash, or update to a page with the data already filled out?
+                end
+            end
+        else 
+            redirect "/login"
+        end
     end
 
     get "/books/:id/edit" do
