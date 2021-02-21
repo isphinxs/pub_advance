a69985e361a8937249ccdcfd501d356864e617ff
Samantha Ostrowski <isphinxs@hotmail.com>
2021-02-20 21:12:55 -0500

    Add routes to BooksController

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,29 @@
 class BooksController < ApplicationController
-    
+    get "/books" do
+        
+    end
+
+    get "/books/new" do
+        
+    end
+
+    get "/books/:id" do
+        
+    end
+
+    post "/books" do
+        
+    end
+
+    get "/books/:id/edit" do
+        
+    end
+
+    patch "/books/:id" do
+        
+    end
+
+    delte "/books/:id" do
+        
+    end
 end
