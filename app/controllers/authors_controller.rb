b5004ae464d36db01d8ee901a9ca380ef6d9b069
Samantha Ostrowski <isphinxs@hotmail.com>
2021-02-20 23:05:48 -0500

    Add routes to AuthorsController

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,3 +1,21 @@
 class AuthorsController < ApplicationController
+    get "/authors" do
+        
+    end
 
+    get "/authors/:id" do
+        
+    end
+
+    get "/authors/:id/edit" do
+        
+    end
+
+    patch "/authors/:id" do
+        
+    end
+
+    delete "/authors/:id" do
+        
+    end
 end
