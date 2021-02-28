b5c3dcfab62a4414868f8cba4597d46949dcca6c
Samantha Ostrowski <isphinxs@hotmail.com>
2021-02-28 18:29:54 -0500

    Update for deployment to Heroku

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require "dotenv/load"
 require "bundler/setup"
 Bundler.require(:default, ENV["SINATRA_ENV"])
 
-set :database, {adapter: "sqlite3", database: "db/#{ENV["SINATRA_ENV"]}.sqlite"}
+# set :database, {adapter: "sqlite3", database: "db/#{ENV["SINATRA_ENV"]}.sqlite"}
+set :database_file, "./database.yml"
 
 require_all "app"
