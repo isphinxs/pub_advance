2d8084f48c0e40bdfce82ebc1e9c9a3fca392dd9
Samantha Ostrowski <isphinxs@hotmail.com>
2021-03-13 18:48:18 -0500

    Add redirect if unauthorized method to Author class

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -21,11 +21,8 @@ class AuthorsController < ApplicationController
     get "/authors/:slug/edit" do
         redirect_if_not_logged_in
         @author = Author.find_by_slug(params[:slug])
-        if @author == current_author
-            erb :"authors/edit"
-        else
-            redirect "/authors", flash[:message] = "There was an error. Please try again." # flash, or update to a page that says you're not permitted to edit/author doesn't exist?
-        end
+        redirect_if_not_authorized(@author)
+        erb :"authors/edit"
     end
 
     patch "/authors/:slug" do
@@ -35,9 +32,7 @@ class AuthorsController < ApplicationController
         name = params[:name]
         username = params[:username]
 
-        if author != current_author
-            redirect "/authors", flash[:message] = "There was an error. Please try again."
-        end
+        redirect_if_not_authorized(author)
 
         if name.blank? || username.blank?
             redirect "/authors/#{slug}/edit", flash[:message] = "All fields must be filled out."
@@ -55,13 +50,17 @@ class AuthorsController < ApplicationController
         redirect_if_not_logged_in
         slug = params[:slug]
         author = Author.find_by_slug(slug)
-        if author == current_author
-            # what to do with author's books?
-            Author.delete(author.id)
-            session.clear
-            redirect "/", flash[:message] = "You have successfully deleted your account"
-        else
-            redirect "/books/#{slug}", flash[:message] = "There was an error deleting the account. Please try again."
+        redirect_if_not_authorized(author)
+        Author.delete(author.id)
+        session.clear
+        redirect "/", flash[:message] = "You have successfully deleted your account"
+    end
+
+    private 
+
+    def redirect_if_not_authorized(author)
+        if author != current_author
+            redirect "/authors", flash[:message] = "There was an error. Please try again."
         end
     end
 end
